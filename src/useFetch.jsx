91d0b2195e3073af900b225abe5cd7b1e3e76382
Ayushhgupta39 <guptaayush3903@outlook.com>
2022-12-15 09:58:28 +0530

    Till #25

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -1,30 +1,38 @@
 import { useEffect, useState } from "react";
 
 const useFetch = (url) => {
-    const [data, setData] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(false);
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-    useEffect(() => {
-        fetch(url)
-          .then((response) => {
-            if(!response.ok) {
-              throw Error('Could not fetch data for that resource');
-            }
-            return response.json();
-          })
-          .then((data) => {
-            setData(data);
-            setIsLoading(false);
-            setError(null);
-          })
-          .catch((err) => {
-            setError(err.message);
-            setIsLoading(false);
-          })
-      }, [url]);
+  useEffect(() => {
+    const abortConst = new AbortController();
 
-      return { data, isLoading, error }
-}
+    fetch(url, { signal: abortConst.signal })
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("Could not fetch data for that resource");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        if (err.name === "AbortError") {
+          console.log("fetch aborted");
+        } else {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => abortConst.abort();
+  }, [url]);
+
+  return { data, isLoading, error };
+};
 
 export default useFetch;
